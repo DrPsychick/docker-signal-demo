d44e2df86f0270d6a67e360079e5e37f0726d825
DrPsychick <drpsychick@drsick.de>
2020-09-23 00:31:28 +0200

    fix: deploy

--- deploy.sh
+++ deploy.sh
@@ -9,8 +9,7 @@ echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin &> /dev/null |
 # push dev image (only latest)
 if [ "$TRAVIS_BRANCH" = "dev" -a "$UBUNTU_VERSION" = "latest" ]; then
   echo "build and push docker image for version $IMAGE:dev"
-  docker build --progress plain --build-arg UBUNTU_VERSION=$UBUNTU_VERSION \
-    -t $IMAGE:dev  .
+  docker build --build-arg UBUNTU_VERSION=$UBUNTU_VERSION -t $IMAGE:dev  .
   docker push $IMAGE:dev
 fi
 
@@ -18,7 +17,6 @@ fi
 if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]; then
   # tag including UBUNTU version
   echo "build and push docker image for version $IMAGE:$UBUNTU_VERSION"
-  docker build --progress plain --build-arg UBUNTU_VERSION=$UBUNTU_VERSION \
-    -t $IMAGE:$UBUNTU_VERSION .
+  docker build --build-arg UBUNTU_VERSION=$UBUNTU_VERSION -t $IMAGE:$UBUNTU_VERSION .
   docker push $IMAGE:$UBUNTU_VERSION
 fi
